5cd4b2c3788be2f61a37aad99281a83741e0a9cb
zahid-bracu <zahid.mym@gmail.com>
2024-09-01 13:13:36 +0600

    ok

--- AlgorithmAndDataStructureProject2022/src/Array/Traverse.java
+++
@@ -1,8 +0,0 @@
-package Array;
-public class Traverse {
-    public static void main(String [] args){
-        int array[]={22,110,55,11,33,66,77,44,99,88};
-        for(int i=0;i<array.length;i++)
-            System.out.print(array[i]);
-    }
-}
